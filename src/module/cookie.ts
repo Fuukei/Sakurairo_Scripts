73f810db6d44b116328386ee1f50aa4f6f5b790e
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-03-29 20:40:40 +0800

    feat(module):add cookie

---
+++ src/module/cookie.ts
@@ -0,0 +1,27 @@
+/**
+ * from github.com/mirai-mamori/Sakurairo ./js/sakura-app.js ,commit 71f5a0c
+ * @license GPL-v2
+ * @date 2021.03
+ */
+function setCookie(key: string, value: string, days?: number) {
+    let expires = "";
+    if (days) {
+        expires = "; expires=" + new Date(Date.now() + (days * 24 * 60 * 60 * 1000)).toUTCString();
+    }
+    document.cookie = key + mashiro_option.cookie_version_control + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(key: string) {
+    const nameEQ = key + mashiro_option.cookie_version_control + "=",
+        ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function removeCookie(key:string) {
+    document.cookie = key + mashiro_option.cookie_version_control + '=; Max-Age=-99999999;';
+}
