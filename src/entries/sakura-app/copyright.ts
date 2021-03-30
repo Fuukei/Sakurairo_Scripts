9accc866badc9c2a760d7fb8fd2267ab81e223e1
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-03-30 13:39:10 +0800

    split add_copyright

---
+++ src/entries/sakura-app/copyright.ts
@@ -0,0 +1,44 @@
+/**
+ * sakura-app.js L943-963
+ */
+import addComment from "./addComment";
+
+const txt = [
+    "# 商业转载请联系作者获得授权，非商业转载请注明出处。",
+    "# For commercial use, please contact the author for authorization. For non-commercial use, please indicate the source.",
+    "# 协议(License)：署名-非商业性使用-相同方式共享 4.0 国际 (CC BY-NC-SA 4.0)",
+    "# 作者(Author)：" + mashiro_option.author_name,
+    "# 链接(URL)：" + window.location.href,
+    "# 来源(Source)：" + mashiro_option.site_name,
+
+]
+type WindowWithClipboardData = Window & typeof globalThis & { clipboardData: DataTransfer }
+function setClipboardText(event: ClipboardEvent, selectionTxt: string) {
+    event.preventDefault();
+    let htmlData = txt.join('<br>') + "<br><br>" + selectionTxt.replace(/\r\n/g, "<br>"),
+        textData = txt.join('\n') + "\n\n" + selectionTxt.toString().replace(/\r\n/g, "\n");
+    if (event.clipboardData) {
+        event.clipboardData.setData("text/html", htmlData);
+        event.clipboardData.setData("text/plain", textData);
+    } else if ((window as WindowWithClipboardData).clipboardData) {
+        //deprecate?
+        return (window as WindowWithClipboardData).clipboardData.setData("text", textData);
+    }
+}
+function copytext(e: ClipboardEvent) {
+    const selection = window.getSelection()
+    if (selection) {
+        const selectionText = selection.toString()
+        if (selectionText.length > 30 && mashiro_option.clipboardCopyright) {
+            setClipboardText(e, selectionText);
+            addComment.createButterbar("复制成功！<br>Copied to clipboard successfully!", 1000);
+        }
+    }
+}
+/**
+ * 添加复制时的版权提示
+ */
+export default function add_copyright() {
+    document.body.removeEventListener("copy", copytext);
+    document.body.addEventListener("copy", copytext);
+}
