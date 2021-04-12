af126a1b185eeac0681177d52db70b55f04c1e1a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-04-12 19:56:11 +0800

    拆分：cookie

--- src/entries/sakura-app/index.js
+++ src/entries/sakura-app/index.js
@@ -57,30 +57,7 @@ mashiro_global.ini = new function () {
     }
 }
 
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + mashiro_option.cookie_version_control + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-    let nameEQ = name + mashiro_option.cookie_version_control + "=",
-        ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function removeCookie(name) {
-    document.cookie = name + mashiro_option.cookie_version_control + '=; Max-Age=-99999999;';
-}
+const {setCookie,getCookie,removeCookie} = require('../../module/cookie')
 
 function imgError(ele, type) {
     switch (type) {
