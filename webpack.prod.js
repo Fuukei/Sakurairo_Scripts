bd5e76b0fb52cca2756c285c359302366b897dc9
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-04-12 20:19:31 +0800

    dev: 自动生成gzip版本

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,27 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
-  optimization:{
-    minimizer:[
+  optimization: {
+    minimizer: [
       new TerserPlugin({
         parallel: true,
         terserOptions: {
           // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-          sourceMap:true,
+          sourceMap: true,
         },
       })
     ]
-  },devtool:"hidden-source-map",output:{
-    iife:true// 是否添加 IIFE 外层
-  }
+  }, devtool: "hidden-source-map",
+  output: {
+    iife: true// 是否添加 IIFE 外层
+  }, plugins: [new CompressionPlugin({
+    test: /\.js(\?.*)?$/i,
+    threshold: 8192,
+
+  })],
+
 })
