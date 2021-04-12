b2f29117911128591ffa945a33a24414ced30e3a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-04-13 00:43:55 +0800

    refactor(AddComment):转换到typescript

--- src/entries/sakura-app/AddComment.ts
+++ src/entries/sakura-app/AddComment.ts
@@ -1,16 +1,14 @@
 const AddComment = {
-    respondId: null,
-    moveForm: function (commId, parentId, respondId) {
-        const __cancel = document.getElementById('cancel-comment-reply-link'),
-            __cancel_text = __cancel.text(),
+    respondId: '',
+    moveForm: function (commId: string, parentId: any, respondId: string) {
+        const 
             t = this,
             comm = document.getElementById(commId),
             respond = document.getElementById(respondId),
             cancel = document.getElementById('cancel-comment-reply-link'),
-            parent = document.getElementById('comment_parent')
+            parent = document.getElementById('comment_parent') as HTMLInputElement
         let div
         //post = document.getElementById('comment_post_ID');
-        __cancel.text(__cancel_text);
         t.respondId = respondId;
         if (!document.getElementById('wp-temp-form-div')) {
             div = document.createElement('div');
@@ -19,29 +17,31 @@ const AddComment = {
             respond.parentNode.insertBefore(div, respond)
         }
         if (!comm) {
-            const temp = document.getElementById('wp-temp-form-div')
-            document.getElementById('comment_parent').value = '0'
+            const temp = document.getElementById('wp-temp-form-div');
+           ( document.getElementById('comment_parent') as HTMLInputElement).value = '0'
             temp.parentNode.insertBefore(respond, temp)
             temp.remove()
         } else {
             comm.parentNode.insertBefore(respond, comm.nextSibling);
         }
-        jQuery("body").animate({
-            scrollTop: jQuery('#respond').offset().top - 180
-        }, 400);
+        const _respond = document.getElementById("respond");
+        window.scrollTo({
+            top: _respond.getBoundingClientRect().top + window.pageYOffset - _respond.clientTop - 100,
+            behavior: "smooth"
+        });
         parent.value = parentId;
         cancel.style.display = '';
-        cancel.onclick = function () {
-            var t = addComment,
+        cancel.onclick = function (e) {
+            var t = AddComment,
                 temp = document.getElementById('wp-temp-form-div'),
                 respond = document.getElementById(t.respondId);
-            document.getElementById('comment_parent').value = '0';
+            (document.getElementById('comment_parent') as HTMLInputElement).value = '0';
             if (temp && respond) {
                 temp.parentNode.insertBefore(respond, temp);
                 temp.remove();
                 //temp.parentNode.removeChild(temp);
             }
-            this.style.display = 'none';
+            (this as HTMLElement).style.display = 'none';
             this.onclick = null;
             return false;
         };
@@ -58,7 +58,7 @@ const AddComment = {
             }
         }
     },
-    createButterbar: function (message, showtime) {
+    createButterbar: function (message: string, showtime: number | undefined) {
         const t = this;
         t.clearButterbar();
         document.body.insertAdjacentHTML('beforeend', '<div class="butterBar butterBar--center"><p class="butterBar-message">' + message + '</p></div>');
