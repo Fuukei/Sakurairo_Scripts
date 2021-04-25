3b5503c82e3bcf9b5f8a02a0c84f3c596397ce76
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-04-25 19:11:31 +0800

    整理可能需要导出到window的函数并导出

---
+++ src/entries/sakura-app/global-func.js
@@ -0,0 +1,82 @@
+function imgError(ele, type) {
+    switch (type) {
+        case 1:
+            ele.src = 'https://view.moezx.cc/images/2017/12/30/Transparent_Akkarin.th.jpg';
+            break;
+        case 2:
+            ele.src = 'https://sdn.geekzu.org/avatar/?s=80&d=mm&r=g';
+            break;
+        default:
+            ele.src = 'https://view.moezx.cc/images/2018/05/13/image-404.png';
+    }
+}
+function cmt_showPopup(ele) {
+    let popup = ele.querySelector("#thePopup");
+    popup.classList.add("show");
+    ele.querySelector("input").onblur = () => {
+        popup.classList.remove("show");
+    }
+}
+/*视频feature*/
+function coverVideo() {
+    let video = document.getElementById("coverVideo"),
+        btn = document.getElementById("coverVideo-btn");
+
+    if (video.paused) {
+        video.play();
+        try {
+            btn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+        } catch { };
+        //console.info('play:coverVideo()');
+    } else {
+        video.pause();
+        try {
+            btn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+        } catch { };
+        //console.info('pause:coverVideo()');
+    }
+}
+
+function killCoverVideo() {
+    var video = document.getElementById("coverVideo");
+    var btn = document.getElementById("coverVideo-btn");
+
+    if (video.paused) {
+        //console.info('none:killCoverVideo()');
+    } else {
+        video.pause();
+        try {
+            btn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+        } catch (e) { };
+        //console.info('pause:killCoverVideo()');
+    }
+}
+function mail_me() {
+    var mail = "mailto:" + mashiro_option.email_name + "@" + mashiro_option.email_domain;
+    window.open(mail);
+}
+/* 首页下拉箭头 */
+function headertop_down() {
+    let coverOffset = document.getElementById("content").getBoundingClientRect().top + window.pageYOffset;
+    window.scrollTo({
+        top: coverOffset,
+        behavior: "smooth"
+    });
+}
+const motionEles = [".bili", ".menhera", ".tieba"];
+function motionSwitch(ele) {
+    for (let i = 0; i < motionEles.length; i++) {
+        document.querySelector(motionEles[i] + '-bar').classList.remove('on-hover');
+        document.querySelector(motionEles[i] + '-container').style.display = 'none';
+    }
+    document.querySelector(ele + '-bar').classList.add("on-hover");
+    document.querySelector(ele + '-container').style.display = 'block';
+}
+
+window.imgError = imgError
+window.cmt_showPopup=cmt_showPopup
+window.coverVideo=coverVideo
+window.killCoverVideo=killCoverVideo
+window.mail_me=mail_me
+window.headertop_down = headertop_down
+window.motionSwitch = motionSwitch
--- src/entries/sakura-app/index.js
+++ src/entries/sakura-app/index.js
@@ -58,19 +58,6 @@ mashiro_global.ini = new function () {
 }
 const buildAPI = require('./api').default
 const { setCookie, getCookie, removeCookie, } = require('../../module/cookie')
-/* function imgError(ele, type) {
-    switch (type) {
-        case 1:
-            ele.src = 'https://view.moezx.cc/images/2017/12/30/Transparent_Akkarin.th.jpg';
-            break;
-        case 2:
-            ele.src = 'https://sdn.geekzu.org/avatar/?s=80&d=mm&r=g';
-            break;
-        default:
-            ele.src = 'https://view.moezx.cc/images/2018/05/13/image-404.png';
-    }
-} */
-
 function slideToogle(el, duration = 1000, mode = '', callback) {
     let dom = el;
     dom.status = dom.status || getComputedStyle(dom, null)['display'];
@@ -313,15 +300,6 @@ function original_emoji_click() {
     })
 }
 original_emoji_click();
-
-
-function cmt_showPopup(ele) {
-    let popup = ele.querySelector("#thePopup");
-    popup.classList.add("show");
-    ele.querySelector("input").onblur = () => {
-        popup.classList.remove("show");
-    }
-}
 
 function scrollBar() {
     if (document.body.clientWidth > 860) {
@@ -594,41 +572,6 @@ function timeSeriesReload(flag) {
 
 timeSeriesReload();
 
-/*视频feature*/
-function coverVideo() {
-    let video = document.getElementById("coverVideo"),
-        btn = document.getElementById("coverVideo-btn");
-
-    if (video.paused) {
-        video.play();
-        try {
-            btn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
-        } catch { };
-        //console.info('play:coverVideo()');
-    } else {
-        video.pause();
-        try {
-            btn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-        } catch { };
-        //console.info('pause:coverVideo()');
-    }
-}
-
-function killCoverVideo() {
-    var video = document.getElementById("coverVideo");
-    var btn = document.getElementById("coverVideo-btn");
-
-    if (video.paused) {
-        //console.info('none:killCoverVideo()');
-    } else {
-        video.pause();
-        try {
-            btn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-        } catch (e) { };
-        //console.info('pause:killCoverVideo()');
-    }
-}
-
 function loadHls() {
     const video = document.getElementById('coverVideo'),
         video_src = document.getElementById("coverVideo").getAttribute("data-src");
@@ -820,16 +763,6 @@ POWERMODE.colorful = true;
 POWERMODE.shake = false;
 document.body.addEventListener('input', POWERMODE);
 
-const motionEles = [".bili", ".menhera", ".tieba"];
-function motionSwitch(ele) {
-    for (let i = 0; i < motionEles.length; i++) {
-        document.querySelector(motionEles[i] + '-bar').classList.remove('on-hover');
-        document.querySelector(motionEles[i] + '-container').style.display = 'none';
-    }
-    document.querySelector(ele + '-bar').classList.add("on-hover");
-    document.querySelector(ele + '-container').style.display = 'block';
-}
-window.motionSwitch = motionSwitch
 
 let comt = document.getElementsByClassName("comt-addsmilies");
 if (comt.length > 0) {
@@ -1068,11 +1001,6 @@ function getqqinfo() {
         // cached.filter('#author').val(user_name);
         setCookie('user_author', user_name, 30);
     });
-}
-
-function mail_me() {
-    var mail = "mailto:" + mashiro_option.email_name + "@" + mashiro_option.email_domain;
-    window.open(mail);
 }
 
 function activate_widget() {
@@ -1921,16 +1849,6 @@ if ((isWebkit || isOpera || isIe) && document.getElementById && window.addEventL
     }, false);
 }
 
-/* 首页下拉箭头 */
-function headertop_down() {
-    let coverOffset = document.getElementById("content").getBoundingClientRect().top + window.pageYOffset;
-    window.scrollTo({
-        top: coverOffset,
-        behavior: "smooth"
-    });
-}
-window.headertop_down = headertop_down
-
 window.addEventListener('load', () => {
     const preload = document.getElementById("preload");
     if (!preload) return;
@@ -2007,3 +1925,4 @@ function web_audio() {
         })
     }
 }
+require('./global-func')
