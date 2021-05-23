65f45d2bd24e24606de36ac69467b0077d1b6472
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-05-23 13:27:14 +0800

    fix: grin is not defined

--- src/entries/sakura-app/index.js
+++ src/entries/sakura-app/index.js
@@ -922,39 +922,6 @@ function smileBoxToggle() {
 }
 smileBoxToggle();
 
-function grin(tag, type, before, after) {
-    let myField;
-    switch (type) {
-        case "custom": tag = before + tag + after; break;
-        case "Img": tag = '[img]' + tag + '[/img]'; break;
-        case "Math": tag = ' {{' + tag + '}} '; break;
-        case "tieba": tag = ' ::' + tag + ':: '; break;
-        default: tag = ' :' + tag + ': ';
-    }
-    if (document.getElementById('comment') && document.getElementById('comment').type == 'textarea') {
-        myField = document.getElementById('comment');
-    } else {
-        return false;
-    }
-    if (document.selection) {
-        myField.focus();
-        sel = document.selection.createRange();
-        sel.text = tag;
-        myField.focus();
-    } else if (myField.selectionStart || myField.selectionStart == '0') {
-        let startPos = myField.selectionStart,
-            endPos = myField.selectionEnd,
-            cursorPos = endPos;
-        myField.value = myField.value.substring(0, startPos) + tag + myField.value.substring(endPos, myField.value.length);
-        cursorPos += tag.length;
-        myField.focus();
-        myField.selectionStart = cursorPos;
-        myField.selectionEnd = cursorPos;
-    } else {
-        myField.value += tag;
-        myField.focus();
-    }
-}
 add_copyright()
 ready(() => {
     getqqinfo();
