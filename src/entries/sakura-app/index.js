88940de8d070b22d28a99fd86c511e1d2095d907
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-05-23 14:37:14 +0800

    fix:(web audio):h is not defined

--- src/entries/sakura-app/index.js
+++ src/entries/sakura-app/index.js
@@ -2008,46 +2008,56 @@ function web_audio() {
             window.AudioContext = window.AudioContext || window.webkitAudioContext,
                 function () {
                     if (window.AudioContext) {
-                        let e = new AudioContext,
+                        let ctx = new AudioContext,
                             t = "880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 659 698 659 698 1046 659 1046 1046 1046 987 698 698 987 987 880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 698 1046 987 1046 1174 1174 1174 1046 1046 880 987 784 880 1046 1174 1318 1174 1318 1567 1046 987 1046 1318 1318 1174 784 784 880 1046 987 1174 1046 784 784 1396 1318 1174 659 1318 1046 1318 1760 1567 1567 1318 1174 1046 1046 1174 1046 1174 1567 1318 1318 1760 1567 1318 1174 1046 1046 1174 1046 1174 987 880 880 987 880".split(" "),//天空之城
                             /*t = "329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 329.628 293.665 293.665 329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 293.665 261.626 261.626 293.665 293.665 329.628 261.626 293.665 329.628 349.228 329.628 261.626 293.665 329.628 349.228 329.628 293.665 261.626 293.665 195.998 329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 293.665 261.626 261.626".split(" "),欢乐颂*/
                             i = 0,
-                            o = 1, dom,
+                            o = 1,
+                            dom,
                             a = "♪ ♩ ♫ ♬ ♭ € § ¶ ♯".split(" "),
-                            n = !1,
-                            select = document.querySelectorAll(".site-title, #moblieGoTop, .site-branding, .searchbox, .changeSkin-gear, .menu-list li");
-                        select.forEach((s) => {
-                            s.addEventListener("mouseenter", (y) => {
+                            selects = document.querySelectorAll(".site-title, #moblieGoTop, .site-branding, .searchbox, .changeSkin-gear, .menu-list li");
+                        selects.forEach((select) => {
+                            select.addEventListener("mouseenter", (e) => {
                                 if (dom) return;
                                 let r = t[i]
-                                r || (i = 0, r = t[i]), i += o
-                                let c = e.createOscillator(),
-                                    l = e.createGain();
-                                if (c.connect(l), l.connect(e.destination), c.type = "sine", c.frequency.value = r, l.gain.setValueAtTime(0, e.currentTime), l.gain.linearRampToValueAtTime(1, e.currentTime + .01), c.start(e.currentTime), l.gain.exponentialRampToValueAtTime(.001, e.currentTime + 1), c.stop(e.currentTime + 1), n = !0) {
-                                    let d = Math.round(7 * Math.random());
-                                    dom = document.createElement("b");
-                                    dom.textContent = a[d],
-                                        h = y.pageX,
-                                        p = y.pageY - 5;
-                                    dom.style.zIndex = "99999";
-                                    dom.style.top = p - 100 + "px";
-                                    dom.style.left = h + "px";
-                                    dom.style.position = "absolute";
-                                    dom.style.color = "#FF6EB4";
-                                    document.body.appendChild(dom);
-                                    dom.animate([
-                                        { top: p + "px" },
-                                        { opacity: 0 }
-                                    ], {
-                                        duration: 500
-                                    })
-                                    setTimeout(() => {
-                                        dom.remove();
-                                        dom = null;
-                                    }, 500)
-                                    y.stopPropagation();
+                                if (!r) {
+                                    (i = 0, r = t[i])
                                 }
-                                n = !1
+                                i += o
+                                const c = ctx.createOscillator(),
+                                    l = ctx.createGain();
+                                c.connect(l)
+                                l.connect(ctx.destination)
+                                c.type = "sine"
+                                c.frequency.value = r
+                                l.gain.setValueAtTime(0, ctx.currentTime)
+                                l.gain.linearRampToValueAtTime(1, ctx.currentTime + .01)
+                                c.start(ctx.currentTime)
+                                l.gain.exponentialRampToValueAtTime(.001, ctx.currentTime + 1)
+                                c.stop(ctx.currentTime + 1)
+                                const d = Math.round(7 * Math.random());
+                                const h = e.pageX
+                                const p = e.pageY - 5
+                                dom = document.createElement("b");
+                                dom.textContent = a[d]
+                                dom.style.zIndex = "99999";
+                                dom.style.top = p - 100 + "px";
+                                dom.style.left = h + "px";
+                                dom.style.position = "absolute";
+                                dom.style.color = "#FF6EB4";
+                                document.body.appendChild(dom);
+                                dom.animate([
+                                    { top: p + "px" },
+                                    { opacity: 0 }
+                                ], {
+                                    duration: 500
+                                })
+                                setTimeout(() => {
+                                    dom.remove();
+                                    dom = null;
+                                }, 500)
+                                e.stopPropagation();
+
                             })
                         })
                     }
