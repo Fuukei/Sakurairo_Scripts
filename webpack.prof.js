48c6a564b07cea151bdf5659036278852e23ac32
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-05-27 11:09:54 +0800

    dev:添加依赖；添加脚本'profile'

---
+++ webpack.prof.js
@@ -0,0 +1,7 @@
+const { merge } = require('webpack-merge')
+const prod = require('./webpack.prod.js')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(prod, {
+    plugins:[...prod.plugins,new BundleAnalyzerPlugin()]
+})
