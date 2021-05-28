d9f6149ab9c64583d361f1afb55ac44ddb073e6a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-05-28 11:31:49 +0800

    fix:onlyOnceATime在内部函数抛出后没有消除状态 feat:支持Promise

--- src/module/util.ts
+++ src/module/util.ts
@@ -3,10 +3,20 @@ export const onlyOnceATime = <T extends Function>(func: T) => {
     return (...args: any) => {
         if (!isRunning) {
             isRunning = true
-            func(...args)
-            isRunning = false
+            try {
+                const result = func(...args)
+                if (result instanceof Promise) {
+                    result.finally(() => isRunning = false)
+                } else {
+                    isRunning = false
+                }
+                return result
+            } catch (e) {
+                console.warn(e)
+                isRunning = false
+            }
         }
     }
 }
-export const max = (a:number,b:number)=>a>b?a:b
-export const min = (a:number,b:number)=>a<b?a:b
+export const max = (a: number, b: number) => a > b ? a : b
+export const min = (a: number, b: number) => a < b ? a : b
