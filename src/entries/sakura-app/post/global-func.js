4af512005756a90251d47415220ad5246e66dabc
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-06-03 17:54:11 +0800

    refactor: 移动表情相关的全局函数

---
+++ src/entries/sakura-app/post/global-func.js
@@ -0,0 +1,45 @@
+
+const motionEles = [".bili", ".menhera", ".tieba"];
+function motionSwitch(ele) {
+    for (let i = 0; i < motionEles.length; i++) {
+        document.querySelector(motionEles[i] + '-bar').classList.remove('on-hover');
+        document.querySelector(motionEles[i] + '-container').style.display = 'none';
+    }
+    document.querySelector(ele + '-bar').classList.add("on-hover");
+    document.querySelector(ele + '-container').style.display = 'block';
+}
+function grin(tag, type, before, after) {
+    let myField;
+    switch (type) {
+        case "custom": tag = before + tag + after; break;
+        case "Img": tag = '[img]' + tag + '[/img]'; break;
+        case "Math": tag = ' {{' + tag + '}} '; break;
+        case "tieba": tag = ' ::' + tag + ':: '; break;
+        default: tag = ' :' + tag + ': ';
+    }
+    if (document.getElementById('comment') && document.getElementById('comment').type == 'textarea') {
+        myField = document.getElementById('comment');
+    } else {
+        return false;
+    }
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = tag;
+        myField.focus();
+    } else if (myField.selectionStart || myField.selectionStart == '0') {
+        let startPos = myField.selectionStart,
+            endPos = myField.selectionEnd,
+            cursorPos = endPos;
+        myField.value = myField.value.substring(0, startPos) + tag + myField.value.substring(endPos, myField.value.length);
+        cursorPos += tag.length;
+        myField.focus();
+        myField.selectionStart = cursorPos;
+        myField.selectionEnd = cursorPos;
+    } else {
+        myField.value += tag;
+        myField.focus();
+    }
+}
+window.motionSwitch = motionSwitch
+window.grin = grin
