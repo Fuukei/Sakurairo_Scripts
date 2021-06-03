43ac5db88b126299256df675eb6d1299cbc9c5df
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-06-03 16:51:08 +0800

    feat: 调整ready()对readyState的检查，确保在DOMContentLoaded阶段执行传入的函数

--- src/module/util.ts
+++ src/module/util.ts
@@ -18,5 +18,19 @@ export const onlyOnceATime = <T extends Function>(func: T) => {
         }
     }
 }
+/**
+ * 延迟函数到DOM树加载完成后执行
+ * @seealso https://developer.mozilla.org/zh-CN/docs/Web/API/Document/readyState
+ * @param fn 要延迟执行的函数
+ */
+export const ready = function (fn:Function) {
+    //interactive:等价于事件DOMContentLoaded
+    //complete:等价于事件load
+    if (document.readyState !== 'loading') {
+        return fn();
+    }
+    //@ts-ignore
+    document.addEventListener('DOMContentLoaded', fn, false);
+};
 export const max = (a: number, b: number) => a > b ? a : b
 export const min = (a: number, b: number) => a < b ? a : b
