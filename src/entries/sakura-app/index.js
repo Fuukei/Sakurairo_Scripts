8647b64ab966e56c4f71933b4cfd45d5572a781e
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-06-03 18:33:10 +0800

    refactor: 移出add_upload_tips

--- src/entries/sakura-app/index.js
+++ src/entries/sakura-app/index.js
@@ -113,90 +113,6 @@ function post_list_show_animation() {
 }
 import { initFontControl, loadFontSetting } from './font_control'
 
-/**
- * 上传图片提示
- */
-function attach_image() {
-    let cached = document.getElementsByClassName("insert-image-tips")[0],
-        upload_img = document.getElementById('upload-img-file');
-    if (!upload_img) return;
-    upload_img.addEventListener("change", (function () {
-        if (this.files.length > 10) {
-            createButterbar("每次上传上限为10张.<br>10 files max per request.");
-            return 0;
-        }
-        for (let i = 0; i < this.files.length; i++) {
-            if (this.files[i].size >= 5242880) {
-                alert('图片上传大小限制为5 MB.\n5 MB max per file.\n\n「' + this.files[i].name + '」\n\n这张图太大啦~请重新上传噢！\nThis image is too large~Please reupload!');
-                return;
-            }
-        }
-        for (let i = 0; i < this.files.length; i++) {
-            let f = this.files[i],
-                formData = new FormData(),
-                xhr = new XMLHttpRequest();
-            formData.append('cmt_img_file', f);
-            xhr.addEventListener('loadstart', function () {
-                cached.innerHTML = '<i class="fa fa-spinner rotating" aria-hidden="true"></i>';
-                createButterbar("上传中...<br>Uploading...");
-            });
-            xhr.open("POST", buildAPI(Poi.api + 'sakura/v1/image/upload'), true);
-            xhr.send(formData);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
-                    cached.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
-                    setTimeout(function () {
-                        cached.innerHTML = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
-                    }, 1000);
-                    let res = JSON.parse(xhr.responseText);
-                    if (res.status == 200) {
-                        let get_the_url = res.proxy;
-                        document.getElementById("upload-img-show").insertAdjacentHTML('afterend', '<img class="lazyload upload-image-preview" src="https://cdn.jsdelivr.net/gh/Fuukei/Public_Repository@latest/vision/theme/colorful/load/inload.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />');
-                        lazyload();
-                        createButterbar("图片上传成功~<br>Uploaded successfully~");
-                        grin(get_the_url, type = 'Img');
-                    } else {
-                        createButterbar("上传失败！<br>Uploaded failed!<br> 文件名/Filename: " + f.name + "<br>code: " + res.status + "<br>" + res.message, 3000);
-                    }
-                } else if (xhr.readyState == 4) {
-                    cached.innerHTML = '<i class="fa fa-times" aria-hidden="true" style="color:red"></i>';
-                    alert("上传失败，请重试.\nUpload failed, please try again.");
-                    setTimeout(function () {
-                        cached.innerHTML = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
-                    }, 1000);
-                }
-            }
-        };
-    }));
-}
-
-/**
- * 添加上传图片的提示
- */
-function add_upload_tips() {
-    const form_submit = document.querySelector('.form-submit #submit');
-    if (form_submit == null) return;
-    if (!mashiro_option.comment_upload_img) {
-        form_submit.style.width = '100%'
-        return
-    }
-    form_submit.insertAdjacentHTML('afterend', '<div class="insert-image-tips popup"><i class="fa fa-picture-o" aria-hidden="true"></i><span class="insert-img-popuptext" id="uploadTipPopup">上传图片</span></div><input id="upload-img-file" type="file" accept="image/*" multiple="multiple" class="insert-image-button">');
-    attach_image();
-
-    const file_submit = document.getElementById('upload-img-file'),
-        hover = document.getElementsByClassName('insert-image-tips')[0],
-        Tip = document.getElementById('uploadTipPopup');
-    if (!file_submit) return;
-    file_submit.addEventListener("mouseenter", function () {
-        hover.classList.toggle('insert-image-tips-hover');
-        Tip.classList.toggle('show');
-    });
-    file_submit.addEventListener("mouseleave", function () {
-        hover.classList.toggle('insert-image-tips-hover');
-        Tip.classList.toggle('show');
-    });
-}
-
 function scrollBar() {
     if (document.body.clientWidth > 860) {
         window.addEventListener("scroll", () => {
@@ -485,7 +401,6 @@ function tableOfContentScroll(flag) {
 }
 tableOfContentScroll(true);
 const pjaxInit = function () {
-    add_upload_tips();
     no_right_click();
     loadFontSetting()
     let _p = document.getElementsByTagName("p");
@@ -1359,7 +1274,6 @@ ready(function () {
     close_SkinMenu && close_SkinMenu.addEventListener("click", function () {
         closeSkinMenu();
     })
-    add_upload_tips();
 });
 ready(function () {
     auto_height();
