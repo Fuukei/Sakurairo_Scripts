a3dab4f60bbcd9b6d0f483291544c24e782bfd8b
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-07-17 00:36:48 +0800

    Update mobile_nav.ts

--- src/sakura-app/mobile_nav.ts
+++ src/sakura-app/mobile_nav.ts
@@ -8,6 +8,7 @@ function toggle(){
     mainContainer.classList.toggle("open");
     document.getElementById("mo-nav").classList.toggle("open");
     document.querySelector(".openNav").classList.toggle("open");
+    document.querySelector(".site-header").classList.toggle("open")
 }
 export function open() {
     toggle()
@@ -16,6 +17,5 @@ export function open() {
 }
 export function close() {
     toggle()
-    document.documentElement.style.overflow = ""
     mainContainer.removeEventListener('click',mainContainer_navOpened_clicked)
 }
