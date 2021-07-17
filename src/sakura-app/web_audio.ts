d19654d95dbb36a01afde00df2996ddbcdb14cc3
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-07-17 18:15:19 +0800

    feat(web_audio): 可定制乐谱、主音量调节

--- src/sakura-app/web_audio.ts
+++ src/sakura-app/web_audio.ts
@@ -1,9 +1,10 @@
+import { sakurairoGlobal } from "./sakurairo_global";
 export function web_audio() {
-    if (mashiro_option.audio) {
-        window.AudioContext = window.AudioContext || window.webkitAudioContext
-        if (window.AudioContext) {
-            let ctx = new AudioContext,
-                t = "880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 659 698 659 698 1046 659 1046 1046 1046 987 698 698 987 987 880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 698 1046 987 1046 1174 1174 1174 1046 1046 880 987 784 880 1046 1174 1318 1174 1318 1567 1046 987 1046 1318 1318 1174 784 784 880 1046 987 1174 1046 784 784 1396 1318 1174 659 1318 1046 1318 1760 1567 1567 1318 1174 1046 1046 1174 1046 1174 1567 1318 1318 1760 1567 1318 1174 1046 1046 1174 1046 1174 987 880 880 987 880".split(" "),//天空之城
+        //@ts-ignore
+        const AudioContext = window.AudioContext || window.webkitAudioContext
+        if (AudioContext) {
+            let ctx: AudioContext = new AudioContext,
+                t = readSheetOption() || sheet,
                 i = 0,
                 o = 1,
                 dom: HTMLElement,
@@ -18,19 +19,22 @@ export function web_audio() {
                     }
                     i += o
                     const c = ctx.createOscillator(),
-                        l = ctx.createGain();
+                        l = ctx.createGain(),
+                        mainGain = ctx.createGain();
                     c.connect(l)
-                    l.connect(ctx.destination)
+                    l.connect(mainGain)
+                    mainGain.connect(ctx.destination)
+                    mainGain.gain.setValueAtTime(sakurairoGlobal.opt.web_audio?.main_gain || 1,ctx.currentTime)
                     c.type = "sine"
-                    c.frequency.value = r
+                    c.frequency.value = r as unknown as number
                     l.gain.setValueAtTime(0, ctx.currentTime)
                     l.gain.linearRampToValueAtTime(1, ctx.currentTime + .01)
                     c.start(ctx.currentTime)
                     l.gain.exponentialRampToValueAtTime(.001, ctx.currentTime + 1)
                     c.stop(ctx.currentTime + 1)
                     const d = Math.round(7 * Math.random());
-                    const h = e.pageX
-                    const p = e.pageY - 5
+                    const h = (e as MouseEvent).pageX
+                    const p = (e as MouseEvent).pageY - 5
                     dom = document.createElement("b");
                     dom.textContent = a[d]
                     dom.style.zIndex = "99999";
@@ -54,8 +58,16 @@ export function web_audio() {
                 })
             })
         }
+}
+//欢乐颂
+/*t = "329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 329.628 293.665 293.665 329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 293.665 261.626 261.626 293.665 293.665 329.628 261.626 293.665 329.628 349.228 329.628 261.626 293.665 329.628 349.228 329.628 293.665 261.626 293.665 195.998 329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 293.665 261.626 261.626".split(" ")*/
+//天空之城
+const sheet = "880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 659 698 659 698 1046 659 1046 1046 1046 987 698 698 987 987 880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 698 1046 987 1046 1174 1174 1174 1046 1046 880 987 784 880 1046 1174 1318 1174 1318 1567 1046 987 1046 1318 1318 1174 784 784 880 1046 987 1174 1046 784 784 1396 1318 1174 659 1318 1046 1318 1760 1567 1567 1318 1174 1046 1046 1174 1046 1174 1567 1318 1318 1760 1567 1318 1174 1046 1046 1174 1046 1174 987 880 880 987 880".split(" ")
+function readSheetOption() {
+    const sheet = sakurairoGlobal.opt.web_audio?.sheet
+    if (typeof sheet == 'string') {
+        return sheet.split(' ')
+    } else if (sheet instanceof Array) {
+        return sheet
     }
 }
-                /*t = "329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 329.628 293.665 293.665 329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 293.665 261.626 261.626 293.665 293.665 329.628 261.626 293.665 329.628 349.228 329.628 261.626 293.665 329.628 349.228 329.628 293.665 261.626 293.665 195.998 329.628 329.628 349.228 391.995 391.995 349.228 329.628 293.665 261.626 261.626 293.665 329.628 293.665 261.626 261.626".split(" "),欢乐颂*/
-
-const sheet = "880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 659 698 659 698 1046 659 1046 1046 1046 987 698 698 987 987 880 987 1046 987 1046 1318 987 659 659 880 784 880 1046 784 659 698 1046 987 1046 1174 1174 1174 1046 1046 880 987 784 880 1046 1174 1318 1174 1318 1567 1046 987 1046 1318 1318 1174 784 784 880 1046 987 1174 1046 784 784 1396 1318 1174 659 1318 1046 1318 1760 1567 1567 1318 1174 1046 1046 1174 1046 1174 1567 1318 1318 1760 1567 1318 1174 1046 1046 1174 1046 1174 987 880 880 987 880".split(" ")
