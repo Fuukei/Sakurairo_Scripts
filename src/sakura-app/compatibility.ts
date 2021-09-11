55d34938c5eb44124ffd0b96c939e1ce3c927f08
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-09-11 17:18:30 +0800

    feat: 更改从user agent提取版本的正则

--- src/sakura-app/compatibility.ts
+++ src/sakura-app/compatibility.ts
@@ -1,8 +1,10 @@
 const UA = navigator.userAgent
-const reg = UA.indexOf('Chrome') != -1 ? /(Chrome)\/(\d+)/i : /(Firefox|Chrome|Version|Opera)\/(\d+)/i
 const version = UA.match(reg);
 export type VersionName = 'Firefox' | 'Edge' | 'Chrome' | 'Opera' | 'Version'
 export type VersionCheck = Record<VersionName | string, number>
+const reg = /(Firefox|Chrome|Version|Opera|Edg)\/(\d+)/i
 export function isSupported(checkList: VersionCheck) {
-    return version && (parseInt(version[2]) >= checkList[version[1]])
+    return version && checkList[version[1]] /**如果目前还没有版本支持，就填undefined */ && (parseInt(version[2]) >= checkList[version[1]])
 }
+export const Accept_Image = setAcceptImage()
+console.log(Accept_Image)
