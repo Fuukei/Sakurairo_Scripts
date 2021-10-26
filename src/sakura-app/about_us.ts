244ef1f42a1240f2b220bda6c544b0a24bb27516
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-26 15:51:44 +0800

    update banner

--- src/sakura-app/about_us.ts
+++ src/sakura-app/about_us.ts
@@ -1,5 +1,16 @@
 export default function about_us() {
-    console.log('Sakurairo', 'https://github.com/mirai-mamori/Sakurairo')
+    console.log('%c Sakurairo %c %s', 'background:#D05A6E;color:#fffffb', '', 'https://github.com/mirai-mamori/Sakurairo')
+}
+
+const zhHans = "「人生的每个阶段的结束，是另一段漂流的开始。」"
+const zhHant = "「人生的每個階段的結束，是另一段漂流的開始。」"
+
+const VersionHitokoto: Record<string, string> = {
+    ja: "「人生の各段階の終わりは、別の漂流の始まりです。」",
+    en: "「The end of each stage of life is the beginning of another drifting.」",
+}
+
+function _hitokoto() {
     //hitokoto
     const lang = navigator.language
     /**
@@ -32,14 +43,4 @@ export default function about_us() {
             console.log(VersionHitokoto[lang_prefix.toLowerCase()] || VersionHitokoto.en)
         }
     }
-    /* console.log("%c Mashiro %c", "background:#24272A; color:#ffffff", "", "https://2heng.xin/");
-    console.log("%c Github %c", "background:#24272A; color:#ffffff", "", "https://github.com/mashirozx"); */
-}
-
-const zhHans = "「人生的每个阶段的结束，是另一段漂流的开始。」"
-const zhHant = "「人生的每個階段的結束，是另一段漂流的開始。」"
-
-const VersionHitokoto: Record<string, string> = {
-    ja: "「人生の各段階の終わりは、別の漂流の始まりです。」",
-    en: "「The end of each stage of life is the beginning of another drifting.」",
 }
