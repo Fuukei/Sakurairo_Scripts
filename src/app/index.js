8d9ac5b3af48a0dcd06763d72dfc7cbab80cff80
KotoriK <52659125+KotoriK@users.noreply.github.com>
2022-01-16 13:46:10 +0800

    refactor: move scrollBar()

--- src/app/index.js
+++ src/app/index.js
@@ -70,33 +70,7 @@ Poi.pjax && import('@sliphua/pjax').then(({ default: Pjax }) =>
 
 var _mashiro_global_skin_secter = true
 import { initFontControl, loadFontSetting } from './font_control'
-
-function scrollBar() {
-    if (document.body.clientWidth > 860) {
-        window.addEventListener("scroll", () => {
-            let s = document.documentElement.scrollTop || document.body.scrollTop,
-                a = document.documentElement.scrollHeight || document.body.scrollHeight,
-                b = window.innerHeight, c,
-                result = parseInt(s / (a - b) * 100),
-                cached = document.getElementById('bar');
-            cached.style.width = result + "%";
-            /* switch (true) {
-                case (result <= 19): c = '#cccccc'; break;
-                case (result <= 39): c = '#50bcb6'; break;
-                case (result <= 59): c = '#85c440'; break;
-                case (result <= 79): c = '#f2b63c'; break;
-                case (result <= 99): c = '#FF0000'; break;
-                case (result == 100): c = '#5aaadb'; break;
-                default: c = "orange";
-            }
-            cached.style.background = c; */
-            //炫彩scrollbar好像不是很好看，又被php那边的样式强制覆盖了，就先注释掉
-            const skinMenu = document.querySelector(".skin-menu");
-            skinMenu && skinMenu.classList.remove("show");
-        })
-    }
-}
-
+import scrollBar from './scrollbar'
 function checkSkinSecter() {
     if (_mashiro_global_skin_secter === false) {
         const pattern = document.querySelector(".pattern-center"),
