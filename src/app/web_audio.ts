f7dc9d3434135b5efa4b4fea92ca8907ae31c1e3
KotoriK <52659125+KotoriK@users.noreply.github.com>
2022-09-25 12:22:13 +0800

    perf(web_audio): 避免重复创建监听器

--- src/app/web_audio.ts
+++ src/app/web_audio.ts
@@ -4,57 +4,58 @@ export function web_audio() {
     //@ts-ignore
     const AudioContext = window.AudioContext || window.webkitAudioContext
     if (AudioContext) {
-        let ctx: AudioContext = new AudioContext,
-            t = readSheetOption() || sheet,
-            i = 0,
-            o = 1,
-            dom: HTMLElement,
-            a = "♪ ♩ ♫ ♬ ♭ € § ¶ ♯".split(" "),
-            selects = document.querySelectorAll(".site-title, #moblieGoTop, .site-branding, .searchbox, .changeSkin-gear, .menu-list li");
-        selects.forEach((select) => {
-            select.addEventListener("mouseenter", (e) => {
-                if (dom) return;
-                let r = t[i]
-                if (!r) {
-                    (i = 0, r = t[i])
-                }
-                i += o
-                const c = ctx.createOscillator(),
-                    l = ctx.createGain(),
-                    mainGain = ctx.createGain();
-                c.connect(l)
-                l.connect(mainGain)
-                mainGain.connect(ctx.destination)
-                mainGain.gain.setValueAtTime(sakurairoGlobal.opt.web_audio?.main_gain || 1, ctx.currentTime)
-                c.type = "sine"
-                c.frequency.value = r as unknown as number
-                l.gain.setValueAtTime(0, ctx.currentTime)
-                l.gain.linearRampToValueAtTime(1, ctx.currentTime + .01)
-                c.start(ctx.currentTime)
-                l.gain.exponentialRampToValueAtTime(.001, ctx.currentTime + 1)
-                c.stop(ctx.currentTime + 1)
-                const d = Math.round(7 * Math.random());
-                const h = (e as MouseEvent).pageX
-                const p = (e as MouseEvent).pageY - 5
-                dom = document.createElement("b");
-                dom.textContent = a[d]
-                dom.style.zIndex = "99999";
-                dom.style.top = p - 100 + "px";
-                dom.style.left = h + "px";
-                dom.style.position = "absolute";
-                dom.style.color = "#FF6EB4";
-                document.body.appendChild(dom);
-                dom.animate([
-                    { top: p + "px" },
-                    { opacity: 0 }
-                ], {
-                    duration: 500
-                })
-                setTimeout(() => {
-                    dom.remove();
-                    dom = null;
-                }, 500)
+        let i = 0
+        let dom: HTMLElement
+        const o = 1
+        const t = readSheetOption() || sheet
+        const selects = document.querySelectorAll(".site-title, #moblieGoTop, .site-branding, .searchbox, .changeSkin-gear, .menu-list li");
+        const notes = "♪ ♩ ♫ ♬ ♭ € § ¶ ♯".split(" ")
+        const ctx: AudioContext = new AudioContext
+        const listener = (e: MouseEvent) => {
+            if (dom) return;
+            let r = t[i]
+            if (!r) {
+                (i = 0, r = t[i])
+            }
+            i += o
+            const c = ctx.createOscillator(),
+                l = ctx.createGain(),
+                mainGain = ctx.createGain();
+            c.connect(l)
+            l.connect(mainGain)
+            mainGain.connect(ctx.destination)
+            mainGain.gain.setValueAtTime(sakurairoGlobal.opt.web_audio?.main_gain || 1, ctx.currentTime)
+            c.type = "sine"
+            c.frequency.value = r as unknown as number
+            l.gain.setValueAtTime(0, ctx.currentTime)
+            l.gain.linearRampToValueAtTime(1, ctx.currentTime + .01)
+            c.start(ctx.currentTime)
+            l.gain.exponentialRampToValueAtTime(.001, ctx.currentTime + 1)
+            c.stop(ctx.currentTime + 1)
+            const d = Math.round(7 * Math.random());
+            const h = e.pageX
+            const p = e.pageY - 5
+            dom = document.createElement("b");
+            dom.textContent = notes[d]
+            dom.style.zIndex = "99999";
+            dom.style.top = p - 100 + "px";
+            dom.style.left = h + "px";
+            dom.style.position = "absolute";
+            dom.style.color = "#FF6EB4";
+            document.body.appendChild(dom);
+            dom.animate([
+                { top: p + "px" },
+                { opacity: 0 }
+            ], {
+                duration: 500
             })
+            setTimeout(() => {
+                dom.remove();
+                dom = null;
+            }, 500)
+        }
+        selects.forEach((select) => {
+            (select as HTMLElement).addEventListener("mouseenter", listener)
         })
     }
 }
