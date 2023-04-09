b30295fe659128940faf1ee08e525895b7fb69b6
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-04-09 14:19:01 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ const javascript_loader = {
             ['@babel/preset-env',
                 {
                     useBuiltIns: 'usage',
-                    corejs: '3.26',
+                    corejs: '3.29',
                 }
             ]],
         cacheDirectory: true,
