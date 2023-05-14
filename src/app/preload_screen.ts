e404f633bb8a08f1f249b435cc51fe6337574d02
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-05-14 21:52:03 +0800

    perf(preload_screen): 定时自动移除预加载; 减少内存占用

--- src/app/preload_screen.ts
+++ src/app/preload_screen.ts
@@ -1,32 +1,36 @@
 export default function () {
     const preload = document.getElementById("preload");
     if (preload) {
-        if (document.readyState === 'complete') return removeScreen(preload)
-        window.addEventListener('load', () => {
-            removeScreen(preload)
-        })
-    }
-}
-function removeScreen(preload: HTMLElement) {
-    document.documentElement.style.overflowY = 'unset';
-    if (_iro.preload_blur != 0) {
-        try {
-            preload.animate(
-                [
-                    { filter: "blur(0px)", backdropFilter: "blur(10px)", opacity: 1 },
-                    { backdropFilter: "blur(0px)grayscale(0)", opacity: 0.1 },
-                    { opacity: 0, filter: "blur(100px)", }
-                ],
-                { duration: _iro.preload_blur, fill: "forwards", easing: "ease" }
-            ).onfinish = () => {
-                preload.remove()
+        let removed = false
+        const removeScreen = () => {
+            if (removed) return
+            document.documentElement.style.overflowY = 'unset';
+            if (_iro.preload_blur != 0) {
+                try {
+                    preload
+                        .animate(
+                            [
+                                { filter: "blur(0px)", backdropFilter: "blur(10px)", opacity: 1 },
+                                { backdropFilter: "blur(0px)grayscale(0)", opacity: 0.1 },
+                                { opacity: 0, filter: "blur(100px)", }
+                            ],
+                            { duration: _iro.preload_blur, fill: "forwards", easing: "ease" }
+                        )
+                        .onfinish = () => {
+                            preload.remove()
+                        }
+                    return
+                } catch (error) {
+                    console.warn(error)
+                }
             }
-            return
-        } catch (error) {
-            console.warn(error)
+            preload.classList.add('hide');
+            preload.classList.remove('show');
+            removed = true
+            setTimeout(() => preload.remove(), 233);
         }
+        if (document.readyState === 'complete') return removeScreen()
+        window.addEventListener('load', removeScreen, { once: true })
+        setTimeout(removeScreen, 3000)
     }
-    preload.classList.add('hide');
-    preload.classList.remove('show');
-    setTimeout(() => preload.remove(), 233);
 }
