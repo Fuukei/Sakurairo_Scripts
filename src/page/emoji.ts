d2b54cc0455e76f4816072ea2ddbc8f7ae815579
KotoriK <52659125+KotoriK@users.noreply.github.com>
2024-04-10 11:12:39 +0800

    fix(emoji): 应处理元素不存在的情况
    Fix mirai-mamori/Sakurairo#873

--- src/page/emoji.ts
+++ src/page/emoji.ts
@@ -1,36 +1,26 @@
-function smileBoxToggle() {
+export default function prepareEmoji() {
     const ele = document.getElementById("emotion-toggle");
-    ele && ele.addEventListener('click', function () {
+    ele && ele.addEventListener('click', () => {
         document.querySelector('.emotion-toggle-off').classList.toggle("emotion-hide");
         document.querySelector('.emotion-toggle-on').classList.toggle("emotion-show");
         document.querySelector('.emotion-box').classList.toggle("emotion-box-show");
     })
-}
-function addEmojiEventListener() {
+
     const row = document.querySelector('.emotion-box>table tr')
+    if (!row) return
 
-    function emojiPannelSwitcher(elementSwitchTo: HTMLElement) {
-        for (const element of row.querySelectorAll('th')) {
-            const emojiClassName = element.className.match(/(\w+)-bar/)
-
-            if (element === elementSwitchTo) {
-                elementSwitchTo.classList.add('on-hover')
-                document.querySelector<HTMLElement>('.' + emojiClassName[1] + '-container').style.display = 'block'
-            } else {
-                element.classList.remove('on-hover')
-                document.querySelector<HTMLElement>('.' + emojiClassName[1] + '-container').style.display = 'none'
-            }
-
-        }
-    }
     row.addEventListener('click', (e) => {
-        if (e.target instanceof HTMLTableCellElement) {
-            emojiPannelSwitcher(e.target)
+        if ((e.target as HTMLElement).tagName === 'TH') {
+            for (const element of row.querySelectorAll('th')) {
+                const container = document.querySelector<HTMLElement>(`.${element.className.match(/(\S+)-bar/)[1]}-container`)
+                if (element === e.target) {
+                    element.classList.add('on-hover')
+                    container.style.display = 'block'
+                } else {
+                    element.classList.remove('on-hover')
+                    container.style.display = 'none'
+                }
+            }
         }
     })
-}
-
-export default function prepareEmoji() {
-    smileBoxToggle()
-    addEmojiEventListener()
 }
