9d3566c28988b80311f2f7b98dced849508f0040
nicocatxzc <nicocatxzc@163.com>
2025-04-26 10:54:12 +0800

    移除k的逻辑，大于七位才换算

--- src/page/time_archive.js
+++ src/page/time_archive.js
@@ -312,8 +312,6 @@ export default function initTimeArchive() {
 function toFixNum(num) {
     if (num >= 1000000) {
       return (num / 1000000).toFixed(2) + 'm';
-    } else if (num >= 100000) {
-      return (num / 1000).toFixed(2) + 'k';
     } else {
       return num;
     }
